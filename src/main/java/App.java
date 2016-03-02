cdb468008eb3ffbaafe56bd6a51360a371e59a7a
Chris Houghton and kevin mattison <mattisonii@yahoo.com>
2016-03-01 16:37:46 -0800

    routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,29 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students", (request,response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Student> students = Student.all();
+      model.put("students", students);
+      model.put("template", "templates/students.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      List<Course> courses = Course.all();
+      model.put("courses", courses);
+      model.put("template", "templates/courses.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
